a6722e54a12de4f30d58238ae62bd0bb4e3754cc
Yi0nn <stefhania.noguera@correounivalle.edu.co>
2023-11-06 17:33:23 -0500

    FRONT

--- Avara.py
+++ Avara.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     main()
 
 def avara(matriz):
-    colores = ['white', 'gray', 'orange', 'red', 'pink', 'green', 'blue']
+    colores = ['#FFF6FF', '#5C0057', '#FF00B5', '#B07DFF', '#B07DFF', '#FFAAD6', '#00D1FF']
     cmap = ListedColormap(colores)
 
     nodosExpandidos = 0
@@ -256,8 +256,8 @@ def avara(matriz):
             arrayExpansion.pop(0)
 
     if not nodoMaestro:
-        eg.msgbox(msg="No se encontró una solución con el siguiente input",
-                  title="Resultado", image="images/mallabomberita.png")
+        eg.msgbox(msg="\n\n          LA BOMBERITA NO ENCONTRO LA SOLUCION PARA EL LABERINTO",
+                  title="RESULTADO", image="images/mallabomberita.png")
     else:
         camino = nodoMaestro.encontrarAncestros()
         nodosExpandidos = raiz.nodosExpandidos()
@@ -266,8 +266,8 @@ def avara(matriz):
         # Impresión de resultados
         timeFinal = time.time()
         timeComputing = timeFinal - timeInitial
-        eg.msgbox(msg="Se encontró una solución con los siguientes datos:\n\nNodos expandidos: " + str(nodosExpandidos) + "\nProfundidad del árbol: " +
-                  str(profundidadArbol) + "\nTiempo de ejecución: " + str(timeComputing)[:10] + " segundos\n\nAhora se visualizará el camino que tomaría la Bombera", title="Resultado")
+        eg.msgbox(msg="\n             LA BOMBERITA ENCONTRO UNA SOLUCION CON ESTOS DATOS\n\n  NODOS:   " + str(nodosExpandidos) + "\n  PROFUNDIDAD DEL ARBOL:  " +
+                  str(profundidadArbol) + "\n  TIEMPO: " + str(timeComputing)[:10] + " SEG \n\n                 ESTE FUE EL CAMINO QUE TOMO LA BOMBERITA", title="RESULTADO")
 
         # Visualización de camino
         fig, ax = plt.subplots()
@@ -276,19 +276,19 @@ def avara(matriz):
         plt.yticks([])
         fig = plt.gcf()
         fig.canvas.manager.set_window_title(
-            "Camino de la Bombera para apagar el fuego usando Avara")
-        textoCubeta = ax.text(0.2, 1.05, "Cubeta: " + str(0),
+            "BUSQUEDA AVARA")
+        textoCubeta = ax.text(0.2, 1.05, "NO. CUBETAS: " + str(0)+"\n ",
                                 fontsize=12, ha="center", va="center", transform=ax.transAxes)
-        textoFuego = ax.text(0.8, 1.05, "Fuego: " + str(0),
+        textoFuego = ax.text(0.8, 1.05, "FUEGOS APAGADOS: " + str(0)+"\n ",
                                fontsize=12, ha="center", va="center", transform=ax.transAxes)
 
         plt.pause(0.5)
         for i in camino:
             textoSemillas.remove()
             textoFuego.remove()
-            textoSemillas = ax.text(0.2, 1.05, "Cubeta: " + str(
-                i.cubetas), fontsize=12, ha="center", va="center", transform=ax.transAxes)
-            textoFuego = ax.text(0.8, 1.05, "Fuego: " + str(i.fuego),
+            textoSemillas = ax.text(0.2, 1.05, "NO. CUBETAS: " + str(
+                i.cubetas)+"\n ", fontsize=12, ha="center", va="center", transform=ax.transAxes)
+            textoFuego = ax.text(0.8, 1.05, "FUEGOS APAGADOS: " + str(i.fuego)+"\n ",
                                    fontsize=12, ha="center", va="center", transform=ax.transAxes)
             textoSemillas
             textoFuego
